8a7a2da3ba331d5add75f58bfffe75a5dbbfaab9
João Victor Jesus de Oliveira <143917197+olvvikitor@users.noreply.github.com>
2026-08-20 01:01:58 -0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        for(let i = 0; i <= nums.length; i ++ ){
+            if(nums[i] === nums[i+1]){
+                return true
+            }
+            return false
+        }
+    }
+}
